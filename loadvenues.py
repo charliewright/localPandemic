eee1b2ac481a5bb1b9303befcb32af796a410662
Amedee d'Aboville <amedee.daboville@gmail.com>
2013-11-09 11:38:25 -0500

    cleanup, and got a json file.

--- loadvenues.py
+++ loadvenues.py
@@ -1,5 +1,6 @@
 import foursquare
-import graph
+import json
+
 city = "NYC"
 radius = 1000
 client = foursquare.Foursquare(client_id='34030ZPC2URSHBRGD44MKQMVZBTIKJIG5TA20W1APVSOYARP', client_secret='5M5UT4HQNU2SBE2FWKQQFQY5YQYV5UMCS0DWFMXHNQX4WGW2')
@@ -11,7 +12,20 @@ categories = {
   "shopping" : "4d4b7105d754a06378d81259",
   "travel" : "4d4b7105d754a06379d81259"
 }
-venues = {}
+def to_json(json, category_name):
+  return {
+      'name' : json['name'],
+      'category' : category_name,
+      'lat' : float(json['location']['lat']),
+      'lng' : float(json['location']['lng']),
+      'population' : int(json['stats']['usersCount']),
+      'infected' : 0,
+      'new_population' : int(json['stats']['usersCount']),
+      'new_infected' : 0
+      }
+
+cat_venues = {}
+real_venues = []
 for cat in categories:
   p = {
       'near': city,
@@ -20,4 +34,10 @@ for cat in categories:
       'intent': 'browse',
       'radius': radius
       }
-  venues[cat] = client.venues.search(params=p)['venues']
+  cat_venues[cat] = client.venues.search(params=p)['venues']
+  for venue in cat_venues[cat]:
+    real_venues.append(to_json(venue,cat))
+f = open('nycdata.json', 'w+')
+s = json.dumps(real_venues)
+f.write(s)
+f.close()
